a8f7f38e8f78306bae2dd99870cb83765976d005
Christian Farias <kurisu615@gmail.com>
2018-11-25 18:20:22 -0300

    Implemented: Lectura del archivo.
    Signed-off-by: Christian Farias <kurisu615@gmail.com>

--- app/src/main/java/cl/ucn/disc/dsm/cafa/phodi/activities/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/cafa/phodi/activities/MainActivity.java
@@ -1,11 +1,29 @@
 package cl.ucn.disc.dsm.cafa.phodi.activities;
 
+import android.app.Person;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.JsonReader;
 import android.widget.ListView;
 import android.widget.Toast;
-import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+
 
 import cl.ucn.disc.dsm.cafa.phodi.R;
 import cl.ucn.disc.dsm.cafa.phodi.adapters.PersonaAdapter;
@@ -34,23 +52,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Obtener las personas (Ejemplo):
         // TODO: Asignar la obtencion de personas a otra clase.
-        ArrayList<Persona> personas = new ArrayList<>();
-
-        personas.add(Persona.builder().nombre("Christian").cargo("Academico").telefono("96759922").build());
-        personas.add(Persona.builder().nombre("Lucia").cargo("Academico").telefono("14366699").build());
-        personas.add(Persona.builder().nombre("Juan").cargo("Funcionario").telefono("19192039").build());
-        personas.add(Persona.builder().nombre("Pedro").cargo("Academico").telefono("23948403").build());
-        personas.add(Persona.builder().nombre("Diego").cargo("Funcionario").telefono("12435333").build());
-        personas.add(Persona.builder().nombre("Martin").cargo("Academico").telefono("12766788").build());
-        personas.add(Persona.builder().nombre("Javier").cargo("Academico").telefono("56678904").build());
-        personas.add(Persona.builder().nombre("Paul").cargo("Funcionario").telefono("24648709").build());
-        personas.add(Persona.builder().nombre("David").cargo("Academico").telefono("98673486").build());
-        personas.add(Persona.builder().nombre("Matias").cargo("Funcionario").telefono("99005644").build());
-        personas.add(Persona.builder().nombre("Ramon").cargo("Funcionario").telefono("98086744").build());
-        personas.add(Persona.builder().nombre("Yamir").cargo("Academico").telefono("23557521").build());
 
         // Crear el adaptador.
-        adapter = new PersonaAdapter(this, personas);
+        adapter = new PersonaAdapter(this, Persona.fromJson(getJsons()));
 
         // Asignar el adaptador al listView de la main activity.
         listView = findViewById(R.id.lv_personas);
@@ -63,4 +67,31 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         Toast.makeText(getApplicationContext(), "Cargando lista... ", Toast.LENGTH_LONG).show();
     }
+
+    /**
+     * Lee el archivo y retorna un array de jsons.
+     * @return Un array de jsons.
+     */
+    private JSONArray getJsons(){
+        String json;
+        InputStream istream = null;
+        JSONArray jArray = null;
+        try {
+            istream = getAssets().open("ucn.json");
+            int size = istream.available();
+            byte[] buffer = new byte[size];
+            istream.read(buffer);
+            istream.close();
+
+            json = new String(buffer, "UTF-8");
+            jArray = new JSONArray(json);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return jArray;
+    }
+
+
 }
--- app/src/main/java/cl/ucn/disc/dsm/cafa/phodi/models/Persona.java
+++ app/src/main/java/cl/ucn/disc/dsm/cafa/phodi/models/Persona.java
@@ -1,10 +1,56 @@
 package cl.ucn.disc.dsm.cafa.phodi.models;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Console;
+import java.util.ArrayList;
+
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 public final class Persona {
+
+    public Persona(int id, String nombre, String cargo, String unidad, String email, String telefono, String oficina) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.unidad = unidad;
+        this.email = email;
+        this.telefono = telefono;
+        this.oficina = oficina;
+    }
+
+    public Persona(JSONObject obj) {
+        try {
+            this.id = obj.getInt("id");
+            this.nombre = obj.getString("nombre");
+            this.cargo = obj.getString("cargo");
+            this.unidad = obj.getString("unidad");
+            this.email = obj.getString("email");
+            this.telefono = obj.getString("telefono");
+            this.oficina = obj.getString("oficina");
+
+        } catch (JSONException je) {
+            je.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Persona> fromJson(JSONArray array) {
+        ArrayList<Persona> personas = new ArrayList<>();
+        for (int i = 0; i < array.length(); i++) {
+            try {
+                personas.add(new Persona(array.getJSONObject(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return personas;
+    }
+
+
 
     /**
      * El ID de la persona dentro de la base de datos.
@@ -48,3 +94,4 @@ public final class Persona {
     @Getter
     private String oficina;
 }
+
